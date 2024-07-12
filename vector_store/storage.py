0d2bbfd7508a82bd14b48cf4aca7ccb2d444a2ea
Leon <leon.ericsson@foi.se>
2024-07-12 14:57:32 +0200

    Mindex implementation ready for eval. added option to not save embedding model as part of Mindex pickle

--- vector_store/storage.py
+++ vector_store/storage.py
@@ -1,7 +1,5 @@
 import numpy as np
 from enum import Enum
-
-from jaxtyping import Float
 from typing import List
 
 class SimilarityMetric(Enum):
@@ -9,7 +7,7 @@ class SimilarityMetric(Enum):
     EUCLIDEAN = 1
     MANHATTAN = 2
 
-def cosine_similarity(x, y) -> Float:
+def cosine_similarity(x, y):
     x_norm = x / np.linalg.norm(x, axis=1, keepdims=True)
     y_norm = y / np.linalg.norm(y, axis=1, keepdims=True)
     return x_norm @ y_norm.T
@@ -23,28 +21,69 @@ def manhattan_similarity(x, y):
 sims = [cosine_similarity, euclidean_similarity, manhattan_similarity]
 
 class VectorStorage:
-    def __init__(self, embedder, similarity = SimilarityMetric.COSINE, query_prefix = ""):
-        self.embedder = embedder
-        self.similarity_fn = sims[similarity.value]
-        self.query_prefix = query_prefix
-        self.index = np.zeros((0, embedder.truncated_dim))
-    
-    #TODO: rename? 
+    """
+    A class for efficient storage and retrieval of vector embeddings.
+
+    VectorStorage indexes documents using their vector representations and 
+    provides fast similarity search functionality.
+    """
+    def __init__(self, embedder, similarity = SimilarityMetric.COSINE, query_prefix = "", save_embedder = False):
+        """
+        Initialize the VectorStorage.
+
+        Args:
+            embedder: Object with encode method and truncate_dim attribute.
+            similarity: Similarity metric for comparing embeddings.
+            query_prefix: Prefix added to queries before encoding.
+            save_embedder: If True, save embedder when pickling.
+        """
+        self._embedder = embedder
+        self._similarity_fn = sims[similarity.value]
+        self._query_prefix = query_prefix
+        self._index = np.zeros((0, embedder.truncate_dim))
+        self._save_embedder = save_embedder
+
+
     def index(self, docs: List[str]):
         """
-        Index documents by encoding them with the embedder. Always extends the index.
+        Add documents to the index by encoding them with the embedder.
         """
-        new_embeddings = self.embedder.encode(docs)
-        self.index = np.concatenate([self.index, new_embeddings])
+        new_embeddings = self._embedder.encode(docs)
+        self._index = np.concatenate([self._index, new_embeddings])
+
 
     def search_top_k(self, queries: List[str], k: int = 10):
-        """Batched search for top k similar documents to each query."""
-        queries = [self.query_prefix + query for query in queries]
-        query_embeddings = self.embedder.encode(queries)
-        similarities = self.similarity_fn(query_embeddings, self.index)
+        """Batched search for top k similar entries in index."""
+        queries = [self._query_prefix + query for query in queries]
+        query_embeddings = self._embedder.encode(queries)
+        similarities = self._similarity_fn(query_embeddings, self._index)
 
-        top_k_indices = np.apply_along_axis(lambda x: np.argsort(x)[-k:][::-1], 1, similarities)
+        def top_k_partitioned(x):
+            partitioned = np.argpartition(x, -k)[-k:]
+            return partitioned[np.argsort(x[partitioned])][::-1]
+
+        top_k_indices = np.apply_along_axis(top_k_partitioned, 1, similarities)
 
         row_indices = np.arange(similarities.shape[0])[:, np.newaxis]
         return top_k_indices, similarities[row_indices, top_k_indices]
+    
+
+    def __getstate__(self):
+        """Custom method to define what gets pickled."""
+        state = self.__dict__.copy()
+        if not self._save_embedder:
+            del state['_embedder']
+        return state
+
+
+    def __setstate__(self, state):
+        """Custom method to define how the object is reconstructed when unpickled."""
+        self.__dict__.update(state)
+        if '_save_embedder' not in state or not state['_save_embedder']:
+            self._embedder = None
+
+
+    def set_embedder(self, embedder):
+        """Method to set the embedder after unpickling if it wasn't saved."""
+        self._embedder = embedder
     
